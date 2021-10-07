51e540eb813588bc734479b9643f032221a894fb
LVGP2021 <zerobudgetde@gmail.com>
2021-10-07 17:30:31 +0100

    improvements

--- auto_ape.js
+++ auto_ape.js
@@ -474,8 +474,7 @@ async function checkBSC(tokenOut, tradeAmount, typeOfSell, profitLevel, lossLeve
         process.exit(0)
     }
 
-    // check = await isSafeToken(tokenOut)
-    check = true
+    check = await isSafeToken(tokenOut)
 
     if (check === true) {
         console.log("✅ CONTRACT SAFE!! BUYING " + tokenName + "!")
