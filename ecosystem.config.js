21824a63a0d45d4c8bc39770eefec0f590cf1539
LVGP2021 <zerobudgetde@gmail.com>
2021-10-11 21:30:03 +0100

    added auto update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,13 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+  apps: [{
+    name: "pcs_listener",
+    script: "./pcs_listener.js P",
+    instances: "1",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
     }
-  }
-};
+  }]
+}
